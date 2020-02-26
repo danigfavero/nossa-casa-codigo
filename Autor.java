5cf023cadbfd2bf642e3247ca9b9ed5f31ed5d3c
danigfavero <danifavero@usp.br>
2020-02-26 12:10:21 -0300

    Adiciona autor

---
+++ Autor.java
@@ -0,0 +1,95 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
+
+
+class Autor {
+
+    String nome;
+    String email;
+    String descricao;
+    LocalDateTime instante;
+
+    public Autor(String nome, String email, String descricao) {
+        this.nome = nome;
+        if (emailInvalido(email)) {
+            throw new IllegalArgumentException("Email inválido");
+        }    
+        this.email = email;
+        if (descricaoInvalida(descricao)) {
+            throw new IllegalArgumentException("Descrição muito longa");
+        }    
+        this.descricao = descricao;
+        this.instante = LocalDateTime.now();
+    }
+
+    private boolean descricaoInvalida(String descricao) {
+        return descricao.length() > 400;
+    }
+
+    private boolean emailInvalido(String email) {
+        String definicaoDoPadrao = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@" +
+                                    "((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}" +
+                                    "\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+" +
+                                    "[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern padrao = java.util.regex.Pattern.compile(definicaoDoPadrao);
+        java.util.regex.Matcher comparador = padrao.matcher(email);
+        return !comparador.matches();
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String dataFormatada = this.instante.format(formatador);
+        return "Autor: " + this.nome 
+               + "\nEmail: " + this.email
+               + "\nDescrição: " + this.descricao
+               + "\nCadastro feito em " + dataFormatada;
+    }    
+
+    public static void main(String[] args) {
+
+        LinkedList<Autor> autores = new LinkedList<>();
+
+        try {
+            autores.add(new Autor("Joao", "joao@email.com", "formado na puc"));
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            autores.add(new Autor("Juca", "aaaaa", "especialista"));
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            autores.add(new Autor("Aldo", "aldo@aldo.br", "descricao longa descricao " 
+                                + "longa descricao longa descricao longa descricao longa " 
+                                + "descricao longa descricao longa descricao longa descricao longa " 
+                                + "descricao longa descricao longa descricao longa descricao longa "
+                                + "descricao longa descricao longa descricao longa descricao longa "
+                                + "descricao longa descricao longa descricao longa descricao longa "
+                                + "descricao longa descricao longa descricao longa descricao longa "
+                                + "descricao longa "));
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            autores.add(new Autor());
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        for (Autor autor : autores) {
+            System.out.println(autor);
+        }
+        
+    }
+}
