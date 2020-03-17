645cbb7aa7b0c2b5d39ab3d6b2d4fb3da0e7aa5b
danigfavero <danifavero@usp.br>
2020-03-17 16:49:17 -0300

    Cria abstração para item no carrinho

--- Carrinho.java
+++ Carrinho.java
@@ -3,72 +3,63 @@ import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Iterator;
 
-public class Carrinho implements Iterable<Livro> {
+public class Carrinho implements Iterable<Item> {
 
-    public HashMap<Livro, Integer> carrinho = new HashMap<>();
-
+    public HashMap<String, Item> carrinho = new HashMap<>();
+    
     public void insere(Livro livro) {
         Assert.isNotEmpty(livro, "Livro inválido");
-        carrinho.put(livro, this.getQuantidade(livro) + 1);
+        Item itemAntigo = carrinho.get(livro.getTitulo());
+        
+        if (itemAntigo == null) {
+            carrinho.put(livro.getTitulo(), new Item(livro, 1));
+        } else {
+            itemAntigo.setQuantidade(itemAntigo.getQuantidade() + 1);
+        } 
     }
-
+        
     public void edita(Livro livro, int quantidade) {
         Assert.isNotEmpty(livro, "Livro inválido");
         Assert.assertTrue(quantidade >= 0, "Quantidade inválida");
         if (quantidade == 0) {
-            carrinho.remove(livro);
+            carrinho.remove(livro.getTitulo());
         }
-        carrinho.put(livro, quantidade);
-
-    }
-
-    public int getQuantidade(Livro livro) {
-        Assert.isNotEmpty(livro, "Livro inválido");
-        if (carrinho.get(livro) == null) {
-            return 0;
-        }
-        return carrinho.get(livro);
-    }
-
-    public static BigDecimal totalProduto(BigDecimal preco, int quantidade) {
-        return preco.multiply(new BigDecimal(quantidade));
+        Item item = carrinho.get(livro.getTitulo());
+        item.setQuantidade(quantidade);
     }
 
     public BigDecimal getTotalDoCarrinho() {
         BigDecimal total = new BigDecimal(0);
-        for (Livro livro : this) {
-            BigDecimal totalParcial = totalProduto(livro.getPreco(), this.getQuantidade(livro));
-            total = total.add(totalParcial);
+        for (Item item : this) {
+            total = total.add(item.getPrecoTotal());
         }
         return total;
     }
-
+     
     @Override
-    public Iterator<Livro> iterator() {
-        return carrinho.keySet().iterator();
+    public Iterator<Item> iterator() {
+        return carrinho.values().iterator();
     }
-
+    
     public static void main(String[] args) {
-
+        
         Livro livro1 = new Livro("meu título", "um resumo", "sumário muito lindo", new BigDecimal(30), 200, "ISBN", LocalDate.parse("2020-12-03"), new Categoria("minha categoria"));
         Livro livro2 = new Livro("meu título2", "um resumo", "sumário muito lindo", new BigDecimal(30), 200, "ISBN2", LocalDate.parse("2020-12-03"), new Categoria("minha categoria"));
-
+        
         Carrinho carrinho = new Carrinho();
         carrinho.insere(livro1);
         carrinho.insere(livro1);
         carrinho.insere(livro2);
         carrinho.edita(livro2, 3);
-
+       
         System.out.println("Título \tPreço \tQtd \tTotal");
-        for (Livro livro : carrinho) {
-            BigDecimal preco = livro.getPreco();
-            int quantidade = carrinho.getQuantidade(livro);
-            System.out.print(livro.getTitulo() + "\t");
-            System.out.print(preco + "\t");
-            System.out.print(quantidade + "\t");
-            System.out.println(Carrinho.totalProduto(preco, quantidade));
+        for (Item item : carrinho) {
+            System.out.print(item.getTitulo() + "\t");
+            System.out.print(item.getPrecoUnidade() + "\t");
+            System.out.print(item.getQuantidade() + "\t");
+            System.out.println(item.getPrecoTotal());
         }
-        System.out.println("TOTAL DA COMPRA:\t" + carrinho.getTotalDoCarrinho());
+        System.out.println("TOTAL DA COMPRA:\t" + carrinho.getTotalDoCarrinho());   
     }
-
+    
 }
